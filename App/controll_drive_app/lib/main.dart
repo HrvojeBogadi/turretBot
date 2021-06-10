e56d47f8aeadcbdc16ec9ba0c835842626c6fa30
Hrvoje <admin@Admins-iMac-Pro.local>
2021-06-11 00:49:41 +0200

    Camera interface with dynamic ip adrress on phone

--- App/controll_drive_app/lib/main.dart
+++ App/controll_drive_app/lib/main.dart
@@ -2,26 +2,51 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:math';
+import 'dart:io';
+import 'dart:async';
+
+import 'package:flutter_mjpeg/flutter_mjpeg.dart';
 
 import 'package:control_pad/control_pad.dart';
 
-void main() {
+
+final String url = "192.168.0.23";
+
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
 
+
   SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeRight])
-    .then((value) => runApp(MyApp()));
+    .then((value) => runApp(MyAppState()));
+
+    print('Closing');
 }
 
-class MyApp extends StatelessWidget {
+class MyAppState extends StatefulWidget{
+  _MyApp createState() => _MyApp();
+}
+
+class _MyApp extends State<MyAppState>{
   bool isInTurretMode = false;
-  void onJoystickEvent(double angle, double distance){
-    double x, y;
+  double xLeft, yLeft, xRight, yRight = 0;
+  double resWidth = 1200;
+  double resHeight = 480;
+  String stream = "http://192.168.0.23:8080/cam.mjpg";
+  
+  void onJoystickRightEvent(double angle, double distance){
+    yRight = distance * cos(angle/(180) * pi);
+    xRight = distance * sin(angle/(180) * pi);
 
-    y = distance * cos(angle/(180) * pi);
-    x = distance * sin(angle/(180) * pi);
+    print("Right Joystick: X Cartesian: $xRight, Y Cartesian: $yRight");
+    //socket.write("Right Joystick Position:$yRight");
+  }
 
-    print("X Cartesian: $x, Y Cartesian: $y");
-    //TODO : Send coordinates to RPi
+  void onJoystickLeftEvent(double angle, double distance){
+    yLeft = distance * cos(angle/(180) * pi);
+    xLeft = distance * sin(angle/(180) * pi);
+
+    print("Left Joystick: X Cartesian: $xLeft, Y Cartesian: $yLeft");
+    //socket.write("Right Joystick Position:$xLeft");
   }
 
   void onButtonPress(){
@@ -32,9 +57,7 @@ class MyApp extends StatelessWidget {
       print("Turret Mode Disengaged!");
       isInTurretMode=false;
     }
-
-
-    //TODO : Send Turret Mode Changed to RPi
+    //socket.write("Turret Mode:$isInTurretMode");
   }
 
   @override
@@ -44,31 +67,55 @@ class MyApp extends StatelessWidget {
           fit: StackFit.expand,
           children: <Widget>[
             Container(
-              alignment: AlignmentDirectional.bottomEnd,
+              color: Colors.amber,
+              child: Mjpeg(
+                stream: stream,
+                isLive: true
+              )
+            ),
+            
+            Container(
+              alignment: AlignmentDirectional.bottomStart,
               child: Container(
                 height: 125,
                 width: 200,
-                margin: EdgeInsets.only(right: 25, bottom: 25),
+                margin: EdgeInsets.only(left: 25, bottom: 5),
                 child: JoystickView(
                   size: 125,
                   showArrows: false,
                   innerCircleColor: Colors.blueAccent,
                   backgroundColor: Colors.black87,
-                  onDirectionChanged: onJoystickEvent
+                  onDirectionChanged: onJoystickLeftEvent
                 ),
               ),
             ),
 
             Container(
-              alignment: AlignmentDirectional.bottomStart,
-              margin: EdgeInsets.only(left: 100, bottom: 50),
+              alignment: AlignmentDirectional.bottomEnd,
+              child: Container(
+                height: 125,
+                width: 200,
+                margin: EdgeInsets.only(right: 25, bottom: 5),
+                child: JoystickView(
+                  size: 125,
+                  showArrows: false,
+                  innerCircleColor: Colors.blueAccent,
+                  backgroundColor: Colors.black87,
+                  onDirectionChanged: onJoystickRightEvent
+                ),
+              ),
+            ),
+
+            Container(
+              alignment: AlignmentDirectional.bottomCenter,
+              margin: EdgeInsets.only(bottom: 40),
               child: ElevatedButton(
                 onPressed: onButtonPress,
                 child: Text(
                   "Turret Mode"
                 ),
                 style: ElevatedButton.styleFrom(
-                  primary: Colors.blueAccent
+                  primary: Colors.green,
                 ),
               ),
             )
--- App/controll_drive_app/test/widget_test.dart
+++ App/controll_drive_app/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:controll_drive_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MyAppState());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
